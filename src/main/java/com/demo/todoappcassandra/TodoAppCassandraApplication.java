3daf6ad7bd6cbfd73421ae2ee81883b1c8b8dc7b
naveenkumaran <naveenkumaran.a@gmail.com>
2020-12-16 23:13:16 +0530

    bootstrap project

---
+++ src/main/java/com/demo/todoappcassandra/TodoAppCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.demo.todoappcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/todoappcassandra/TodoAppCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.todoappcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
