b142b21c3a11f208a6b793d9ad51bfee6f7a6913
naveenkumaran <naveenkumaran.a@gmail.com>
2020-12-16 23:18:56 +0530

    Bootstrap cassandra project & gitpod

--- src/main/java/com/demo/todoappcassandra/TodoAppCassandraApplication.java
+++ src/main/java/com/demo/todoappcassandra/TodoAppCassandraApplication.java
@@ -3,7 +3,7 @@ package com.demo.todoappcassandra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { CassandraDataAutoConfiguration.class, CassandraAutoConfiguration.class })
 public class TodoAppCassandraApplication {
 
 	public static void main(String[] args) {
